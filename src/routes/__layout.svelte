a5e477ad9c1ad9ef2e230468fcc07de0f09512a7
NSVEGUR <nsvegur@gmail.com>
2022-02-01 21:34:14 +0530

    initial setup for the project

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Nav from './../components/nav.svelte';
+	import Contact from './../components/contact.svelte';
+</script>
+
+<Nav />
+<slot />
+<Contact />
+
+<style>
+	@import './../global.css';
+</style>
---
+++ src/routes/sponsers.svelte
@@ -0,0 +1,20 @@
+<main>
+	<h1>SAE-Infinite-Challengers</h1>
+	<h2>Sponsers Page</h2>
+</main>
+
+<style>
+	main {
+		display: grid;
+		place-items: center;
+	}
+	h1 {
+		font-family: var(--secondary-font);
+	}
+	h2 {
+		background: url('./brushBg.png') center center;
+		background-size: contain;
+		font-weight: 400;
+		margin: 10px 0;
+	}
+</style>
